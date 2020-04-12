7ad3cd197da8647a5fed42b762fe5a6adfe65208
Ravi Kuril <ravikuril13@gmail.com>
2020-04-12 22:35:15 +0530

    Update UserController.java

--- src/main/java/com/telstra/restservice/controller/UserController.java
+++ src/main/java/com/telstra/restservice/controller/UserController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.telstra.restservice.service.*;
-import antlr.collections.List;
 import javassist.expr.NewArray;
 
 @RestController
